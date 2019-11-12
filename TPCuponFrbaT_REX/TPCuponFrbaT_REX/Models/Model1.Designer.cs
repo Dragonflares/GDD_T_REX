52785de83dc56de21d41d2c3e95ae94deac0451e
Unknown <ivanlose@hotmail.com>
2019-11-12 19:22:09 -0300

    Subo esto antes de estresarme

--- TPCuponFrbaT_REX/TPCuponFrbaT_REX/Models/Model1.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\Documentos\TP GDD\GDD_T_REX\TPCuponFrbaT_REX\TPCuponFrbaT_REX\Models\Model1.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
---
+++ src/FrbaCupon/FrbaCupon/Login/PantallaLogin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaCupon.Login
+{
+    public partial class PantallaLogin : Form
+    {
+        public PantallaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void PantallaLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
