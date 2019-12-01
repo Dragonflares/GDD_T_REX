22a8a65b126853e283ade0337b2919fce4d4c9f7
DanielaRios <daniela_erre@hotmail.com>
2019-12-01 12:44:28 -0300

    Eliminar rol del usuario [T_REX].SP_Eliminar_RolDeUsuario

--- scripts/script_creacion_inicial.sql
+++ scripts/script_creacion_inicial.sql
@@ -1043,3 +1043,19 @@ as
 	select * from [T_REX].ROL
 
 go
+
+-- Eliminar rol del usuario [T_REX].SP_Eliminar_RolDeUsuario
+
+IF OBJECT_ID('T_REX.SP_Eliminar_RolDeUsuario') IS NOT NULL
+    DROP PROCEDURE [T_REX].SP_Eliminar_RolDeUsuario
+GO
+
+CREATE PROCEDURE [T_REX].SP_Eliminar_RolDeUsuario
+	@id_rol int,
+	@id_usuario int
+as
+begin
+	delete from [T_REX].ROL_USUARIO 
+	where id_rol=@id_rol AND id_usuario=@id_usuario; 
+end
+go
