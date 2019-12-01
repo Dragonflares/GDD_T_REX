c6ddf126a6c95423208ed16df8e76aee37cb8508
DanielaRios <daniela_erre@hotmail.com>
2019-12-01 12:39:12 -0300

    CREATE PROCEDURE [T_REX].[UpdateRol]

--- scripts/script_creacion_inicial.sql
+++ scripts/script_creacion_inicial.sql
@@ -1012,3 +1012,22 @@ as
 
 go
 
+-- Modificar rol [T_REX].[UpdateRol]
+
+IF OBJECT_ID('T_REX.UpdateRol') IS NOT NULL
+    DROP PROCEDURE [T_REX].[UpdateRol]
+GO
+
+CREATE PROCEDURE [T_REX].[UpdateRol]
+	@idRol int,
+	@nombre varchar(50),
+	@estado bit
+as 
+begin
+	UPDATE [T_REX].ROL 
+	SET nombre=@nombre, 
+	estado=@estado
+	WHERE id_rol=@idRol
+
+end
+go
