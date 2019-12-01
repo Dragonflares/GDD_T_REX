5172c0838106b5af89f2dd16e516e4615ff9311e
DanielaRios <daniela_erre@hotmail.com>
2019-12-01 12:43:16 -0300

    TODOS LOS ROLES [T_REX].SP_GetAll_Roles

--- scripts/script_creacion_inicial.sql
+++ scripts/script_creacion_inicial.sql
@@ -1031,3 +1031,15 @@ begin
 
 end
 go
+
+-- TODOS LOS ROLES [T_REX].SP_GetAll_Roles
+
+IF OBJECT_ID('T_REX.SP_GetAllRoles') IS NOT NULL
+    DROP PROCEDURE [T_REX].[SP_GetAllRoles]
+GO
+
+CREATE PROCEDURE [T_REX].[SP_GetAllRoles]
+as
+	select * from [T_REX].ROL
+
+go
