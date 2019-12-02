132c914c39204f0d878ffff75339ed0234b94c75
DanielaRios <daniela_erre@hotmail.com>
2019-12-01 21:50:42 -0300

    -- Alta ROL		[T_REX].[SP_Alta_NuevoRol]

--- scripts/script_creacion_inicial.sql
+++ scripts/script_creacion_inicial.sql
@@ -1059,3 +1059,27 @@ begin
 	where id_rol=@id_rol AND id_usuario=@id_usuario; 
 end
 go
+
+-- Alta ROL		[T_REX].[SP_Alta_NuevoRol]
+
+IF OBJECT_ID('T_REX.[SP_Alta_NuevoRol]') IS NOT NULL
+    DROP PROCEDURE [T_REX].[SP_Alta_NuevoRol]
+GO
+CREATE PROCEDURE [T_REX].[SP_Alta_NuevoRol] @nombre nvarchar(50)
+AS
+	declare @resultado nvarchar(10)
+	if not exists (select 1 from [T_REX].ROL where nombre=@nombre)
+		BEGIN
+			insert into [T_REX].ROL (nombre)
+			values (@nombre)
+			
+			SELECT @resultado = max(id_rol)
+			from [T_REX].ROL
+
+		END
+	else set @resultado = 'ERROR'
+
+	select @resultado;
+
+GO
+
