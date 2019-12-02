0081d2cf3d66684bdf69565a20aabe0c1e7b86e7
Dragonflares <ivanlose@hotmail.com>
2019-12-02 07:45:53 -0300

    Alta Rol

--- src/FrbaOfertas/FrbaOfertas/ABMRol/Alta.Designer.cs
+++ src/FrbaOfertas/FrbaOfertas/ABMRol/Alta.Designer.cs
@@ -28,11 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NombreNuevoRol = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Funcionalidades = new System.Windows.Forms.ComboBox();
+            this.btn_atras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 73);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Funcionalidad";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // NombreNuevoRol
+            // 
+            this.NombreNuevoRol.Location = new System.Drawing.Point(118, 24);
+            this.NombreNuevoRol.Margin = new System.Windows.Forms.Padding(4);
+            this.NombreNuevoRol.Name = "NombreNuevoRol";
+            this.NombreNuevoRol.Size = new System.Drawing.Size(216, 22);
+            this.NombreNuevoRol.TabIndex = 2;
+            this.NombreNuevoRol.TextChanged += new System.EventHandler(this.NombreNuevoRol_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 132);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 132);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Funcionalidades
+            // 
+            this.Funcionalidades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Funcionalidades.FormattingEnabled = true;
+            this.Funcionalidades.Location = new System.Drawing.Point(118, 66);
+            this.Funcionalidades.Margin = new System.Windows.Forms.Padding(4);
+            this.Funcionalidades.Name = "Funcionalidades";
+            this.Funcionalidades.Size = new System.Drawing.Size(216, 24);
+            this.Funcionalidades.TabIndex = 3;
+            this.Funcionalidades.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btn_atras
+            // 
+            this.btn_atras.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_atras.Location = new System.Drawing.Point(12, 266);
+            this.btn_atras.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_atras.Name = "btn_atras";
+            this.btn_atras.Size = new System.Drawing.Size(97, 31);
+            this.btn_atras.TabIndex = 22;
+            this.btn_atras.Text = "Atrás";
+            this.btn_atras.UseVisualStyleBackColor = true;
+            this.btn_atras.Click += new System.EventHandler(this.btn_atras_Click);
+            // 
+            // AltaRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Alta";
+            this.ClientSize = new System.Drawing.Size(347, 305);
+            this.Controls.Add(this.btn_atras);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Funcionalidades);
+            this.Controls.Add(this.NombreNuevoRol);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AltaRol";
+            this.Text = "Alta Rol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NombreNuevoRol;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox Funcionalidades;
+        private System.Windows.Forms.Button btn_atras;
     }
 }
--- src/FrbaOfertas/FrbaOfertas/ABMRol/Alta.cs
+++ src/FrbaOfertas/FrbaOfertas/ABMRol/Alta.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FrbaOfertas.ABMRol
 {
@@ -15,6 +16,98 @@ namespace FrbaOfertas.ABMRol
         public Alta(ABMRol abmrol)
         {
             InitializeComponent();
+            this.llenarItems(Funcionalidades);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // SqlConnection connection = Utils.Database.getConnection();
+                //connection.Open();
+                SqlCommand procedure = Utils.Database.createCommand("T_REX.AltaRol");
+                procedure.Parameters.Add("@nombre_rol", SqlDbType.NVarChar).Value = NombreNuevoRol.Text;
+                procedure.Parameters.Add("@activo", SqlDbType.Bit).Value = 1;
+                Utils.Database.executeProcedure(procedure);
+
+                SqlCommand query = Utils.Database.createCommand("SELECT max (id_rol) FROM [T_REX].ROL");
+                int id = Utils.Database.executeScalar(query);
+                SqlCommand procedure2 = Utils.Database.createCommand("T_REX.AgregarFuncionalidadRol");
+                String funcionalidad = Funcionalidades.Text;
+                procedure2.Parameters.Add("@nombreNuevaFuncionalidadRol", SqlDbType.VarChar).Value = Funcionalidades.Text;
+                procedure2.Parameters.Add("@rol_id", SqlDbType.Int).Value = id;
+                Utils.Database.executeProcedure(procedure2);
+                // connection.Close();
+                MessageBox.Show("Alta realizada");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR",
+   MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = "";
+                }
+            }
+
+        }
+
+        public void llenarItems(ComboBox cb)
+        {
+            try
+            {
+
+                SqlConnection connection = Utils.Database.getConnection();
+                connection.Open();
+                SqlCommand sql = Utils.Database.createCommand("SELECT descripcion FROM [T_REX].[FUNCIONALIDAD]");
+                SqlDataReader sdr = sql.ExecuteReader();
+                while (sdr.Read())
+                {
+                    cb.Items.Add(sdr["descripcion"].ToString());
+                }
+                cb.SelectedIndex = 0;
+                sdr.Close();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se lleno el ComboBox: " + ex.ToString());
+            }
+
+
+        }
+
+        private void NombreNuevoRol_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_atras_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
