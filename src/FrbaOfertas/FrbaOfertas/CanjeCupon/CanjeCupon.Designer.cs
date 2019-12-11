74f2d126a9b33f252f3fc5d782815de5d90a80ca
Dragonflares <ivanlose@hotmail.com>
2019-12-11 15:03:52 -0300

    Subo para testear el canjeCupon en la note

--- src/FrbaOfertas/FrbaOfertas/CanjeCupon/CanjeCupon.Designer.cs
+++ src/FrbaOfertas/FrbaOfertas/CanjeCupon/CanjeCupon.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.btn_volver = new System.Windows.Forms.Button();
             this.dgv_cupon = new System.Windows.Forms.DataGridView();
-            this.cod = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.id_compra = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dar_de_baja = new System.Windows.Forms.DataGridViewButtonColumn();
             this.btn_buscar = new System.Windows.Forms.Button();
             this.btn_limpiar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Entregar = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_cupon)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -62,57 +62,19 @@
             this.dgv_cupon.AllowUserToDeleteRows = false;
             this.dgv_cupon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_cupon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.cod,
-            this.id_compra,
-            this.codigo,
             this.id,
-            this.dar_de_baja});
+            this.descripcion,
+            this.cliente,
+            this.codigo,
+            this.Entregar});
             this.dgv_cupon.Location = new System.Drawing.Point(36, 167);
             this.dgv_cupon.Margin = new System.Windows.Forms.Padding(2);
             this.dgv_cupon.Name = "dgv_cupon";
             this.dgv_cupon.ReadOnly = true;
             this.dgv_cupon.RowTemplate.Height = 24;
-            this.dgv_cupon.Size = new System.Drawing.Size(486, 154);
+            this.dgv_cupon.Size = new System.Drawing.Size(494, 154);
             this.dgv_cupon.TabIndex = 30;
             this.dgv_cupon.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_cupon_CellContentClick);
-            // 
-            // cod
-            // 
-            this.cod.HeaderText = "Código";
-            this.cod.Name = "cod";
-            this.cod.ReadOnly = true;
-            this.cod.Width = 120;
-            // 
-            // id_compra
-            // 
-            this.id_compra.HeaderText = "Id Compra";
-            this.id_compra.Name = "id_compra";
-            this.id_compra.ReadOnly = true;
-            // 
-            // codigo
-            // 
-            this.codigo.HeaderText = "Fecha Vencimiento";
-            this.codigo.Name = "codigo";
-            this.codigo.ReadOnly = true;
-            this.codigo.Width = 130;
-            // 
-            // id
-            // 
-            this.id.DataPropertyName = "id";
-            this.id.HeaderText = "id";
-            this.id.Name = "id";
-            this.id.ReadOnly = true;
-            this.id.Visible = false;
-            // 
-            // dar_de_baja
-            // 
-            this.dar_de_baja.HeaderText = "Dar de Baja";
-            this.dar_de_baja.Name = "dar_de_baja";
-            this.dar_de_baja.ReadOnly = true;
-            this.dar_de_baja.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.dar_de_baja.Text = "Dar de Baja";
-            this.dar_de_baja.UseColumnTextForButtonValue = true;
-            this.dar_de_baja.Width = 90;
             // 
             // btn_buscar
             // 
@@ -144,14 +106,14 @@
             this.groupBox1.Controls.Add(this.btn_buscar);
             this.groupBox1.Location = new System.Drawing.Point(37, 27);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(485, 105);
+            this.groupBox1.Size = new System.Drawing.Size(493, 105);
             this.groupBox1.TabIndex = 32;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filtro de Busqueda";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(107, 41);
+            this.textBox1.Location = new System.Drawing.Point(86, 38);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(129, 20);
             this.textBox1.TabIndex = 1;
@@ -159,17 +121,58 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Location = new System.Drawing.Point(30, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Codigo Cupon";
+            this.label1.Text = "Producto";
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Visible = false;
+            // 
+            // descripcion
+            // 
+            this.descripcion.DataPropertyName = "descripcion";
+            this.descripcion.HeaderText = "Producto";
+            this.descripcion.Name = "descripcion";
+            this.descripcion.ReadOnly = true;
+            // 
+            // cliente
+            // 
+            this.cliente.DataPropertyName = "cliente";
+            this.cliente.HeaderText = "Cliente Comprador";
+            this.cliente.Name = "cliente";
+            this.cliente.ReadOnly = true;
+            this.cliente.Width = 120;
+            // 
+            // codigo
+            // 
+            this.codigo.HeaderText = "Fecha Vencimiento";
+            this.codigo.Name = "codigo";
+            this.codigo.ReadOnly = true;
+            this.codigo.Width = 130;
+            // 
+            // Entregar
+            // 
+            this.Entregar.DataPropertyName = "Entregar";
+            this.Entregar.HeaderText = "Entregar";
+            this.Entregar.Name = "Entregar";
+            this.Entregar.ReadOnly = true;
+            this.Entregar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Entregar.Text = "Entregar";
+            this.Entregar.UseColumnTextForButtonValue = true;
+            this.Entregar.Width = 90;
             // 
             // CanjeCupon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 399);
+            this.ClientSize = new System.Drawing.Size(566, 399);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_volver);
             this.Controls.Add(this.dgv_cupon);
@@ -191,10 +194,10 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn cod;
-        private System.Windows.Forms.DataGridViewTextBoxColumn id_compra;
-        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
-        private System.Windows.Forms.DataGridViewButtonColumn dar_de_baja;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
+        private System.Windows.Forms.DataGridViewButtonColumn Entregar;
     }
 }
--- src/FrbaOfertas/FrbaOfertas/CanjeCupon/CanjeCupon.cs
+++ src/FrbaOfertas/FrbaOfertas/CanjeCupon/CanjeCupon.cs
@@ -8,22 +8,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using FrbaOfertas.Models.Usuarios;
+using FrbaOfertas.Models.Proveedores;
+using System.Data.SqlClient;
+
 
 namespace FrbaOfertas.CanjeCupon
 {
     public partial class CanjeCupon : Form
     {
         private Usuario usuario;
+        private Proveedor target;
+
         public CanjeCupon(Usuario user)
         {
             usuario = user;
             InitializeComponent();
+            loadCuponesPorEntregar();
         }
 
         private void btn_volver_Click(object sender, EventArgs e)
         {
-            this.Close();
             this.Owner.Show();
+            this.Close();
+        }
+
+        public void loadCuponesPorEntregar()
+        {
+            string takecupon = "SELECT cup.id_cupon as id, cup.cupon_fecha_deconsumo as fechaVencimiento, " +
+                "offer.descripcion as descripcion, u.username as cliente" +
+                "FROM [GD2C2019].[T_REX].[Cupon] cup " + 
+                "INNER JOIN [GD2C2019].[T_REX].[Oferta] offer ON offer.id_oferta = cup.id_oferta " +
+                "INNER JOIN [GD2C2019].[T_REX].[Compra] comp ON comp.id_compra = cup.id_compra " +
+                "INNER JOIN [GD2C2019].[T_REX].[Cliente] cli ON cli.id_cliente = comp.id_cliente " +
+                "INNER JOIN [GD2C2019].[T_REX].[Usuario] u ON u.id_usuario = cli.id_usuario " +
+                "WHERE p.estado = 1";
+
+            if (!String.IsNullOrEmpty(textBox1.Text)) takecupon += " and lower(of.descripcion) like '%" + textBox1.Text.ToLower() + "%'";
+
+
+
+            takecupon += "ORDER BY [id] ASC";
+            SqlCommand takeClients = FrbaOfertas.Utils.Database.createCommand(takecupon);
+            DataTable table = Utils.Database.getData(takeClients);
+            this.dgv_cupon.DataSource = table;
         }
 
         private void btn_limpiar_Click(object sender, EventArgs e)
@@ -43,6 +70,7 @@ namespace FrbaOfertas.CanjeCupon
                     if (c is MonthCalendar)
                         ((MonthCalendar)c).Visible = false;
                 });
+            loadCuponesPorEntregar();
         }
 
         private void dgv_cupon_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -51,16 +79,16 @@ namespace FrbaOfertas.CanjeCupon
             {
                 return;
             }
-            if (e.ColumnIndex == dgv_cupon.Columns["Dar de Baja"].Index)
+            if (e.ColumnIndex == dgv_cupon.Columns["Entregar"].Index)
             {
-                new FrbaOfertas.CanjeCupon.ListadoClientes();
-                this.Enabled = false;
+                FrbaOfertas.CanjeCupon.ListadoClientes pantalla = new FrbaOfertas.CanjeCupon.ListadoClientes();
+                pantalla.ShowDialog();
             }
         }
 
         private void btn_buscar_Click(object sender, EventArgs e)
         {
-
+            loadCuponesPorEntregar();
         }
     }
 }
